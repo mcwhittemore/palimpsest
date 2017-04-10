e38a115e310303ca21e008e9df952fd645c071db
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-04-10 13:20:58 -0400

    improve performence by adding a declare step

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const ndarray = require('ndarray');
-const runIndexer = require('./lib/indexer');
-const toImage = require('./lib/to-image');
 
-var api = module.exports = async function(output, series, indexer) {
-  var index = await runIndexer(series, indexer);
-  console.log('Compiling image');
-  return toImage(output, index);
+const declare = require('./lib/declare');
+const allocate = require('./lib/allocate');
+const assign = require('./lib/assign');
+const paint = require('./lib/paint');
+
+var api = module.exports = function(output, series, indexer) {
+  // create the index and set default values for index.counts and index.data
+  var index = declare(output, indexer);
+
+  // tally up the total value for each key in index.counts
+  allocate(series, index);
+
+  // create the color values for each key in index.data
+  assign(series, index);
+
+  // make sure the colors are ints not floats
+  console.log('----> normalize');
+  index.data.forEach((v, k) => {
+    index.data.set(k, v.map(n => Math.floor(n)));
+  });
+
+  // apply the colors to the output
+  console.log('----> paint');
+  return paint(output, index);
 }
 
 api.open = require('./lib/open-series');
